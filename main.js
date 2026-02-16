1aa5a18742ff903ebbede5cda278369652d427df
neurolabusc <rorden@sc.edu>
2026-02-16 16:32:38 -0500

    New UI

--- main.js
+++ main.js
@@ -1,71 +1,52 @@
 import './style.css'
-import { Niivue, SLICE_TYPE, SHOW_RENDER, MULTIPLANAR_TYPE } from '@niivue/niivue'
-import { Niimath } from '@niivue/niimath'
-import { mat3, vec3 } from 'gl-matrix'
+import { Niivue, NVImage, SHOW_RENDER } from '@niivue/niivue'
 
 async function main() {
-  async function addVolumeFromFiles(f) {
-    console.log('attempting to open ', f[0].name)
-    console.log('details', f[0])
-    while (nv.volumes.length > 1) {
-      nv.removeVolume(nv.volumes[1])
-    }
-    await nv.loadFromFile(f[0])
-    nv.setColormap(nv.volumes[1].id, 'green2cyan');
-  }
-  openBtn.onclick = function () {
-    let input = document.createElement('input')
-    input.style.display = 'none'
-    input.type = 'file'
-    document.body.appendChild(input)
-    input.onchange = function (event) {
-      addVolumeFromFiles(event.target.files)
-    }
-    input.click()
-  }
-  aboutBtn.onclick = function () {
-    window.alert(
-      "NiiNav shows scalp locations."
-    )
-  }
-  shaderSelect.onchange = function () {
-    nv.setMeshShader(1, shaderSelect.value)
-  }
-  const meshList = [{ url: './standard_1005.jcon' }, { url: './scalp.mz3' }]
-  async function handleImageLoaded(data) {
-    return
-    if (nv.volumes.length < 2) {
-      return
-    }
-    await nv.loadMeshes(meshList)
-    nv.setMeshShader(0, "Rim")
-    nv.setMeshShader(1, "Silhouette")
-    //nv.setMeshShader(1, "Outline")
-  }
-  var nv = new Niivue({
-    show3Dcrosshair: true,
-    backColor: [1, 1, 1, 1],
-    onImageLoaded: handleImageLoaded,
-  })
+  const nv = new Niivue({ show3Dcrosshair: true, backColor: [1, 1, 1, 1] })
   await nv.attachTo("gl")
-  let shaders = nv.meshShaderNames()
-  console.log(`..`, shaders)
-  for (const shader of nv.meshShaderNames()) {
-    const option = document.createElement('option')
-    option.value = shader
-    option.textContent = shader.charAt(0).toUpperCase() + shader.slice(1)
-    shaderSelect.appendChild(option)
+  // State variables
+  let brainImg, scalpImg, lesionImg;
+  // 1. Unified function to refresh the viewer
+  function updateDisplay() {
+    const mode = renderMode.selectedIndex;
+    while (nv.volumes.length > 0) nv.removeVolume(nv.volumes[0]);
+    nv.addVolume(mode > 1 ? brainImg : scalpImg);
+    nv.addVolume(lesionImg);
+    nv.volumes[1].opacity = (mode % 2 !== 0) ? 1 : 0;
+    nv.updateGLVolume();
   }
-
-  nv.setSliceType(nv.sliceTypeRender)
-  nv.opts.multiplanarShowRender = SHOW_RENDER.ALWAYS
-  nv.opts.meshXRay = 0.2
-  const volumeList = [{ url: './mni152.nii.gz' }, { url: './lesion.nii.gz', colormap: "green2cyan"}]
-  await nv.loadVolumes(volumeList)
-    await nv.loadMeshes(meshList)
-    nv.setMeshShader(0, "Rim")
-    nv.setMeshShader(1, "Silhouette")
-
+  // 2. Unified helper for clicking a button to load a file
+  function setupFilePicker(btn, onLoaded, colormap = "gray") {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.style.display = 'none';
+    document.body.appendChild(input);
+    btn.onclick = () => input.click();
+    input.onchange = async (e) => {
+      const file = e.target.files[0];
+      if (!file) return;
+      const img = await NVImage.loadFromFile({ file, colormap });
+      onLoaded(img);
+      updateDisplay();
+    };
+  }
+  // 3. Assign button behaviors
+  setupFilePicker(openBrainBtn, (img) => brainImg = img);
+  setupFilePicker(openScalpBtn, (img) => scalpImg = img);
+  setupFilePicker(openLesionBtn, (img) => lesionImg = img, "green2cyan");
+  // 4. Initial loading
+  renderMode.onchange = updateDisplay;
+  [scalpImg, brainImg, lesionImg] = await Promise.all([
+    NVImage.loadFromUrl({ url: './T1_116.nii.gz' }),
+    NVImage.loadFromUrl({ url: './bT1_116.nii.gz' }),
+    NVImage.loadFromUrl({ url: './LESION_116.nii.gz', colormap: "green2cyan" })
+  ]);
+  nv.setSliceType(nv.sliceTypeRender);
+  nv.opts.multiplanarShowRender = SHOW_RENDER.ALWAYS;
+  nv.opts.meshXRay = 0.05;
+  updateDisplay();
+  await nv.loadMeshes([{ url: './standard_1005.jcon' }]);
+  nv.setMeshShader(0, "Rim");
 }
 
-main()
+main();
